9b58fedfcaf73229e7767f3d0ce7bd0a45b61400
ahmadkhan-afzali <ahmadkhanafzaly22@gmail.com>
2025-05-11 12:10:08 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -2,8 +2,13 @@
 import React from "react";
 import { View,Text } from "react-native";
 
+const App =()=>{
+  return(
 <View>
 
-  <Text style={{backgroundColor:"blue", fontFamily:"newroman"}}> Hi</Text>
+  <Text style={{backgroundColor:"blue", fontFamily:""}}> Hi</Text>
   
   </View>
+  )
+}
+  export default App;
