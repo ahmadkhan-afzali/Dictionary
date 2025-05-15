f22c6c3c377ee06db74f6d51585163617ed2edff
ahmadkhan-afzali <ahmadkhanafzaly22@gmail.com>
2025-05-15 13:07:20 +0530

    dictionary index page

--- src/screen/login.jsx
+++ src/screen/login.jsx
@@ -1,16 +1,23 @@
-import {SafeAreaView, StyleSheet, Text, useColorScheme, View} from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
 import React from 'react';
 
 const login = () => {
-  const theme = useColorScheme()
-  const isDarkMode = theme ==='dark';
+  const theme = useColorScheme();
+  const isDarkMode = theme === 'dark';
 
-  const backgroundColor = isDarkMode ? "black" :"white";
-  const textColor = isDarkMode ? "white" :"black";
+  const backgroundColor = isDarkMode ? 'black' : 'white';
+  const textColor = isDarkMode ? 'white' : 'black';
 
-  return (    
-    <SafeAreaView style={[styles.container,{backgroundColor:backgroundColor}]}>
-      <Text style={[styles.txt,{color:textColor}]}>Login</Text>     
+  return (
+    <SafeAreaView
+      style={[styles.container, {backgroundColor: backgroundColor}]}>
+      <Text style={[styles.textColor, {color: textColor}]}>Login</Text>
     </SafeAreaView>
   );
 };
@@ -19,10 +26,9 @@ export default login;
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
-    justifyContent:"center",
-    alignItems:"center",
-    
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 
   textColor: {
